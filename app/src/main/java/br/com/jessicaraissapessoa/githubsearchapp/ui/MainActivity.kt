a7a42dac537f4e9461b4a6c707037aeb0aabd5cd
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-10-12 00:52:59 -0300

    MainActivity.kt -> Mudança para package UI, preenchimento com esqueleto dos métodos, declaração das variáveis, onCreate()

---
+++ app/src/main/java/br/com/jessicaraissapessoa/githubsearchapp/ui/MainActivity.kt
@@ -0,0 +1,105 @@
+package br.com.jessicaraissapessoa.githubsearchapp.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import br.com.jessicaraissapessoa.githubsearchapp.R
+import br.com.jessicaraissapessoa.githubsearchapp.data.GitHubService
+import br.com.jessicaraissapessoa.githubsearchapp.domain.Repository
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var nomeUsuario: EditText
+    lateinit var btnConfirmar: Button
+    lateinit var listaRepositories: RecyclerView
+    lateinit var githubApi: GitHubService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayShowHomeEnabled(true) //Habilitando exibição do ícone do app
+
+        setContentView(R.layout.activity_main)
+        setupView()
+        showUserName()
+        setupRetrofit()
+        getAllReposByUserName()
+    }
+
+    fun setupView() { // Metodo responsavel por realizar o setup da view e recuperar os Ids do layout
+        nomeUsuario = findViewById(R.id.et_nome_usuario)
+        btnConfirmar = findViewById(R.id.btn_pesquisar)
+        listaRepositories = findViewById(R.id.rv_lista_repositories)
+    }
+
+    private fun setupListeners() { //metodo responsavel por configurar os listeners click da tela
+        btnConfirmar.setOnClickListener {
+
+        }
+    }
+
+
+    // salvar o usuario preenchido no EditText utilizando uma SharedPreferences
+    private fun saveUserLocal() {
+        //@TODO 3 - Persistir o usuario preenchido na editText com a SharedPref no listener do botao salvar
+    }
+
+    private fun showUserName() {
+        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+    }
+
+    //Metodo responsavel por fazer a configuracao base do Retrofit
+    fun setupRetrofit() {
+        /*
+           @TODO 5 -  realizar a Configuracao base do retrofit
+           Documentacao oficial do retrofit - https://square.github.io/retrofit/
+           URL_BASE da API do  GitHub= https://api.github.com/
+           lembre-se de utilizar o GsonConverterFactory mostrado no curso
+        */
+    }
+
+    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
+    fun getAllReposByUserName() {
+        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
+    }
+
+    // Metodo responsavel por realizar a configuracao do adapter
+    fun setupAdapter(list: List<Repository>) {
+        /*
+            @TODO 7 - Implementar a configuracao do Adapter , construir o adapter e instancia-lo
+            passando a listagem dos repositorios
+         */
+    }
+
+
+    // Metodo responsavel por compartilhar o link do repositorio selecionado
+    // @Todo 11 - Colocar esse metodo no click do share item do adapter
+    fun shareRepositoryLink(urlRepository: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, urlRepository)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    // Metodo responsavel por abrir o browser com o link informado do repositorio
+
+    // @Todo 12 - Colocar esse metodo no click item do adapter
+    fun openBrowser(urlRepository: String) {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(urlRepository)
+            )
+        )
+
+    }
+
+}
